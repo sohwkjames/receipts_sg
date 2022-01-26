844a2eb253f3c4d7a21471eaaf0c681b2ccf1af9
James <jamessoh11@gmail.com>
2022-01-26 18:34:37 +0800

    remove some code for heroku

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
-// Tmp
+// mongoose.connect(keys.mongoURI);
+// // Tmp
 const app = express();
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [keys.cookieKey]
+//   })
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 authRoutes(app);
 
