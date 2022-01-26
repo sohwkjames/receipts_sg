3f0a5d2f72ec34fbeda8aa6785a40d95d806960f
James <jamessoh11@gmail.com>
2022-01-26 18:42:44 +0800

    Heroku

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-// mongoose.connect(keys.mongoURI);
-// // Tmp
+mongoose.connect(keys.mongoURI);
+// Tmp
 const app = express();
 
-// app.use(
-//   cookieSession({
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//     keys: [keys.cookieKey]
-//   })
-// );
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-authRoutes(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+app.listen(PORT);
